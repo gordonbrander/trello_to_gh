02e2fc503ba31dcb2216c6ba47155bdee7fbf9ec
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-05-12 16:30:52 -0700

    POST to GH API when publish flag present
    Upon success, will move file from cache dir to published dir.
    This way, if you hit the rate limit and need to retry, you can
    `--actions publish` and it will pick up where you left off.

--- trello_to_gh.py
+++ trello_to_gh.py
@@ -3,8 +3,10 @@ import json
 import argparse
 from trello_to_gh import util
 from trello_to_gh import trello
-from os import path, makedirs
+import os
+from os import path
 import logging
+import requests
 
 parser = argparse.ArgumentParser(
     description="""A lettersmith plugin.""",
@@ -36,30 +38,86 @@ parser.add_argument(
     default="cache"
 )
 
+LOAD_CACHE = "load_cache"
+PUBLISH = "publish"
+DEFAULT_ACTIONS = (LOAD_CACHE, PUBLISH,)
+
 parser.add_argument(
-    '--dry_run',
-    help="Run script without publishing to GitHub",
-    action="store_true"
+    '--actions',
+    help="""A space-separated list of actions to perform.
+
+By default the script will load what has been read from the export file into the
+cache directory and then publish everything in the cache directory to GitHub.
+
+If you want to do a dry run or prune the issues before publishing, you can
+run this script with ``--actions load_cache``. This will create a cache full
+of .json files that you can edit.
+
+To publish a pruned list, without overwriting the changes you've made,
+run the script again with ``--actions publish``. This will publish anything
+found in the cache directory, without first writing to the cache directory.
+""",
+    choices=DEFAULT_ACTIONS,
+    default=DEFAULT_ACTIONS,
+    nargs='+'
 )
-parser.set_defaults(dry_run=False)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     trello_export = args.export_file
     config = args.config_file
+    credentials = args.credentials_file
     cache_dir = args.cache_dir
-
     # Make sure cache dir exists
-    issues_cache_dir = path.join(cache_dir, "issues_json")
+    issues_queue = path.join(cache_dir, "queue")
+    issues_published = path.join(cache_dir, "published")
+    issues_url = "https://api.github.com/repos/{owner}/{repo}/issues".format(
+        owner=config["github"]["owner"],
+        repo=config["github"]["repo"])
+
     try:
-        makedirs(issues_cache_dir)
+        os.makedirs(issues_queue)
     except FileExistsError:
         pass
 
-    issues = trello.collate_issues(trello_export, config)
+    try:
+        os.makedirs(issues_published)
+    except FileExistsError:
+        pass
 
-    for issue in issues:
-        file_title = util.safe_filename(issue["title"])
-        file_name = "{}.json".format(file_title)
-        with open(path.join(issues_cache_dir, file_name), "w") as f:
-            json.dump(issue, f)
+    if LOAD_CACHE in args.actions:
+        issues = trello.collate_issues(trello_export, config)
+
+        for issue in issues:
+            file_title = util.safe_filename(issue["title"])
+            file_name = "{}.json".format(file_title)
+            with open(path.join(issues_queue, file_name), "w") as f:
+                json.dump(issue, f)
+
+    if PUBLISH in args.actions:
+        # See https://developer.github.com/v3/#authentication
+        params = {"access_token": credentials["oauth_token"]}
+        file_paths = (
+            path.join(issues_queue, file_path)
+            for file_path in os.listdir(issues_queue)
+            if file_path.endswith(".json"))
+        issues = (
+            (file_path, util.read_json(file_path))
+            for file_path in file_paths)
+        for file_path, issue in issues:
+            resp = requests.post(issues_url, json=issue, params=params)
+            if resp.status_code == 201:
+                # On success, move file to published dir
+                os.rename(
+                    file_path,
+                    path.join(issues_published, path.basename(file_path))
+                )
+                # And log success
+                resp_json = resp.json()
+                log = json.dumps({
+                    "url": resp_json["url"],
+                    "id": resp_json["id"]
+                })
+                logging.info(log)
+            else:
+                logging.error(resp.text)
